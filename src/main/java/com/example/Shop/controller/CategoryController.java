c4d5e6db59478755957413cc5a270d1da13dc2d8
Mehedi Hasan <mehedibuet73@gmail.com>
2025-04-18 20:51:17 +0600

    feat(category): added category controller

--- src/main/java/com/example/Shop/controller/CategoryController.java
+++ src/main/java/com/example/Shop/controller/CategoryController.java
@@ -7,9 +7,7 @@ import com.example.Shop.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,16 +16,73 @@ import java.util.List;
 @RequestMapping("${api.prefix}/categories")
 public class CategoryController {
 
-    private final CategoryService categoryService;
+  private final CategoryService categoryService;
 
-    @GetMapping
-    public ResponseEntity<ApiResponse> getAllCategories() {
-        try{
-            List<Category> allCategories = categoryService.getAllCategories();
+  @GetMapping
+  public ResponseEntity<ApiResponse> getAllCategories() {
+    try {
+      List<Category> allCategories = categoryService.getAllCategories();
 
-            return new ResponseEntity<>(new ApiResponse("All Categories", allCategories), HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(new ApiResponse("Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+      return new ResponseEntity<>(new ApiResponse("All Categories", allCategories), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @PostMapping
+  public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
+    try {
+      Category category1 = categoryService.addCategory(category);
+      return new ResponseEntity<>(new ApiResponse("Category Added", category1), HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Error", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/category/{categoryId}/get")
+  public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long categoryId) {
+    try {
+      Category category = categoryService.getCategoryById(categoryId);
+      return new ResponseEntity<>(new ApiResponse("Category Found", category), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Category Not Found", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @GetMapping("/category/{name}/name")
+  public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+    try {
+      Category category = categoryService.getCategoryByName(name);
+      return new ResponseEntity<>(new ApiResponse("Category Found", category), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Category Not Found", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @DeleteMapping("/category/{categoryId}/delete")
+  public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long categoryId) {
+    try {
+      categoryService.deleteCategory(categoryId);
+      return new ResponseEntity<>(new ApiResponse("Category Deleted", null), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Error Deleting Category", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PutMapping("/category/{categoryId}/update")
+  public ResponseEntity<ApiResponse> updateCategory(
+      @PathVariable Long categoryId, @RequestBody Category category) {
+    try {
+      Category category1 = categoryService.updateCategory(categoryId, category);
+      return new ResponseEntity<>(new ApiResponse("Category Updated", category1), HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(
+          new ApiResponse("Error Updating Category", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+  }
 }
